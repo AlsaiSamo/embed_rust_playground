7e98094fe38e4f67f3c4ca1505e1b400badfce88
Alsai Samo <alsai_imikoy@proton.me>
2024-02-05 03:36:40 +0700

    Flake: add scripts

--- flake.nix
+++ flake.nix
@@ -15,99 +15,127 @@
     crane,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [(import rust-overlay)];
-      # Not pictured: trying to use crossSystem.
-      # crossSystem is not required and, at least for embedded rust projects, serves
-      # no purpose.
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-      toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        # Not pictured: trying to use crossSystem.
+        # crossSystem is not required and, at least for embedded rust projects, serves
+        # no purpose.
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
 
-      # Filter to not remove linker scripts
-      linkerScriptFilter = path: type: let
-        baseName = builtins.baseNameOf (toString path);
-      in (pkgs.lib.hasSuffix ".x" baseName);
-      # All filters (crane's and linker script) combined
-      craneFilters = path: type: (linkerScriptFilter path type) || (craneLib.filterCargoSources path type);
+        # Filter to not remove linker scripts
+        linkerScriptFilter = path: type: let
+          baseName = builtins.baseNameOf (toString path);
+        in (pkgs.lib.hasSuffix ".x" baseName);
+        # All filters (crane's and linker script) combined
+        craneFilters = path: type: (linkerScriptFilter path type) || (craneLib.filterCargoSources path type);
 
-      # Generate package along with the apps (scripts) to flash, begin debugging, verify,
-      # etc.
-      # TODO: GDB should have some protection from flashing a program to an incorrect chip
-      # This is possible by also defining the probe name for bmp
-      # TODO: on Darwin, serial port will be different
-      makeProgram = {
-        crateName,
-        gdbName,
-        directory,
-        ...
-      }: let
-        # Verify that the target microcontroller is reachable and is the expected one
-        # TODO: use writeShellApplication instead of writeScript
-        gdbVerify = pkgs.writeScript "gdb_verify.sh" ''
+        #Attaches GDB to BMP
+        #TODO: make it possible for the first argument to not be a path -> use default path.
+        scriptBMP_GDB = pkgs.writeScript "bmp-gdb-attach.sh" ''
           #!/usr/bin/env sh
+          #path to the debugged device
+          SERIAL_PATH=''${1:-'/dev/ttyACM0'}
+          shift
 
-          # Name of the device being manipulated in 'monitor swdp_scan' output.
-          GDB_NAME=$1
-          # Path to serial device
-          SERIAL_PATH=$\{2:-'/dev/ttyACM0'}
-
-          #Scan first to assert that name in GDB is correct
-          #TODO: determine if cutting 8th and 9th fields is ok
-          #(and if it is ok to use cut here at all)
-          DEVICE_NAME=$(gdb -nx --batch \
-              -ex "target extended-remote $SERIAL_PATH" \
-              -ex "monitor swdp_scan" 2>&1 | tail -n 1 | cut --delimiter=\  --fields="8,9" | head --bytes=-1)
-          if [ "$GDB_NAME" = "$DEVICE_NAME" ];
-          then
-              exit 0
-          else
-              echo "Expected swdp_scan to return '$GDB_NAME', got '$DEVICE_NAME'"
-              echo $DEVICE_NAME > out
-              exit 1
+          if [ ! -c "$SERIAL_PATH" ]; then
+            echo "error: $SERIAL_PATH does not exist"
+            exit 1
           fi
+          ${pkgs.gdb}/bin/gdb -ex "target extended-remote $SERIAL_PATH" -ex "monitor swdp_scan" "$@"
         '';
-        #TODO: scripts to flash firmware (add necessary info to function's inputs),
-        #check firmware against file, or simply remain in the session
-      in
-        #TODO: names are "package_name"-"bmp/probe-rs/etc."-"debug/flash/etc."
-        #options for bmp: serial port
-        # (probe only connects one device so attach 1 works always)
-        {
-          # apps."${crateName}-bmp-debug" = {
-          #   type = "app";
-          #   # program =
-          # };
+
+        #Autofills the chip name
+        scriptProbeRsFor = shortName: chipName:
+        #TODO: mention that the script will not work with the command that do not
+        #expect --chip, but these commands are not about the chip anyway, so the user
+        #has to use probe-rs directly
+          pkgs.writeScript "probe-rs-${chipName}.sh" ''
+            #!/usr/bin/env sh
+            if [ $# -eq 0 ]; then
+              # Call probe-rs for help
+              ${pkgs.probe-rs}/bin/probe-rs
+              # Exit with the same code
+              exit 2
+            fi
+            COMMAND=$1
+            shift
+            ${pkgs.probe-rs}/bin/probe-rs $COMMAND --chip ${chipName} "$@"
+          '';
+        scriptOpenOCDFor = shortName: target: CPUTAPID: interface: transport:
+          pkgs.writeScript "openocd-${shortName}.sh" ''
+            #!/usr/bin/env sh
+            ${pkgs.openocd}/bin/openocd -c "set CPUTAPID ${CPUTAPID}" -f interface/${interface} \
+            -c "transport select ${transport}"  -f target/${target} "$@"
+          '';
+        genScripts = list:
+          builtins.foldl' (acc: elem:
+            {
+              "probe-rs-${elem.shortName}" = {
+                type = "app";
+                program = "${scriptProbeRsFor elem.shortName elem.chipName}";
+              };
+              "openocd-${elem.shortName}" = {
+                type = "app";
+                program = "${scriptOpenOCDFor elem.shortName elem.target elem.cputapid elem.interface elem.transport}";
+              };
+            }
+            // acc) {}
+          list;
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+            [
+              cargo-binutils
+              gdb
+              probe-rs
+              #TODO: have a fork be available too?
+              openocd
+            ]
+            ++ [
+              toolchain
+            ];
         };
-      # Map makeProgram to a list of directory names, join the attrsets, then join
-      # it with the attrset containing devshell definition
-      # TODO: think about having devshells (and toolchains) for development
-      # on different architectures (if the programmer wants to develop, say, only for rp2040 here)
-    in {
-      #TODO:
-      # flashing and debugging setup
-      # Can make it as apps.[name], that could be run with nix run
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs;
-          [
-            # gcc
-            cargo-binutils
-            gdb
-          ]
-          ++ [
-            toolchain
+        packages.at32_experiment = craneLib.buildPackage {
+          src = pkgs.lib.cleanSourceWith {
+            src = craneLib.path ./at32f403acgu7_experiment;
+            filter = craneFilters;
+          };
+          cargoLock = ./Cargo.lock;
+          #embedded targets cannot be checked automatically
+          doCheck = false;
+        };
+        apps =
+          {
+            bmp_gdb = {
+              type = "app";
+              program = "${scriptBMP_GDB}";
+            };
+          }
+          // genScripts [
+            {
+              shortName = "at32";
+              chipName = "at32f403acgu7";
+              target = "stm32f1x.cfg";
+              interface = "stlink.cfg";
+              cputapid = "0x2ba01477";
+              transport = "hla_swd";
+              #TODO: is it ok not to have transport here?
+            }
+            #TODO: rp2040 will not work with stlink because stlink cannot do multidrop swd
+            #(which is needed because the chip is multicore).
+            # {
+            #   shortName = "rp2040";
+            #   chipName = "rp2040";
+            #   target = "";
+            #   interface = "";
+            #   cputapid = "";
+            # }
           ];
-      };
-      packages.at32_experiment = craneLib.buildPackage {
-        src = pkgs.lib.cleanSourceWith {
-          src = craneLib.path ./at32f403acgu7_experiment;
-          filter = craneFilters;
-        };
-        cargoLock = ./Cargo.lock;
-        #embedded targets cannot be checked automatically
-        doCheck = false;
-      };
-    });
+      }
+    );
 }
